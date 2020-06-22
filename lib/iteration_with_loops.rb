1b2e1877dea596034e832555347af85f3e605dd3
John Doan <johndoan42@gmail.com>
2020-06-22 14:11:17 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ outer_results = []
 row_index = 0
 while row_index < src.count do
   element_index = 0
-  min_temp = 100
+  min_temp = 1000
   while element_index < src[row_index].count do
     if src[row_index][element_index] < min_temp
       min_temp = src[row_index][element_index]
